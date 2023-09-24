bba82ea00eabe2ea1ff7dd6a3c70b0066fd5e58c
Andrzej Borucki <borucki.andrzej@gmail.com>
2023-09-24 18:36:54 +0200

    tests proportional OK

---
+++ test/testProportionalL.cpp
@@ -0,0 +1,48 @@
+//
+// Created by andrzej on 8/13/23.
+//
+#include "misc/util.h"
+#include "gtest/gtest.h"
+#include "logic/LineView.h"
+#include "logic/LineIndexedDocument.h"
+
+using namespace std;
+using namespace vl;
+
+TEST (ProportionalL, portion) {
+    string fname = "../test/data/proportional.txt";
+    string content = makeContent(fname);
+    auto sizes = getSizes(fname);
+    LineIndexedDocument doc(content.c_str(), content.length());
+    LineView vtest(&doc);
+    vtest.setWrapMode(0);
+    vtest.setScreenLineLen(8);
+    for (int j = 0; j <= doc.lineCount(); j ++)
+        for (int i = 20; i < 40; i += 5) {
+            vtest.setScreenLineCount(i);
+            vtest.gotoProportional((double)j/doc.lineCount());
+            vtest.fillDeque();
+            vtest.recalcLines();
+            ASSERT_EQ(min((size_t)vtest.screenLineCount(), sizes.size()), vtest.size());
+        }
+}
+
+TEST (ProportionalL, reversing) {
+    string fname = "../test/data/textviewer.h0";
+    string content = makeString(fname);
+    auto sizes = getSizesStr(fname);
+    LineIndexedDocument doc(content.c_str(), content.length());
+    LineView vtest(&doc);
+    vtest.setWrapMode(0);
+    vtest.setScreenLineLen(41);
+    vtest.setScreenLineCount(19);
+    int64_t previousStartY = 0;
+    for (int i = 0; i < doc.lineCount(); i += 10) {
+        vtest.gotoProportional((double)i/doc.lineCount());
+        vtest.fillDeque();
+        vtest.recalcLines();
+        int64_t startY = vtest.startY();
+        EXPECT_GE(startY, previousStartY);
+        previousStartY = startY;
+    }
+}
